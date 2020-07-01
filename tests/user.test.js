dee115b77fa935ffa825b0cb3a8fc199ec55ecf6
Rahul Kapoor <rahul253801@gmail.com>
2020-07-01 23:05:38 +0530

    added testcases for delete user, fetch profile

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,21 @@
 const request = require('supertest');
 const app = require('../src/appnew');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const User = require('../src/models/user');
 
+const userId = new mongoose.Types.ObjectId();
+//setup a base user
 const userOne = {
+    _id : userId,
     name : 'Rahul',
     email : 'rahul253@gmail.com',
-    password : 'qwerty253'
+    password : 'qwerty253',
+    tokens : [
+        {
+            token : jwt.sign({ _id : userId }, process.env.JWT_SECRET)
+        }
+    ]
 }
 
 //it runs before each test case
@@ -21,28 +31,60 @@ beforeEach(async () => {
 // });
 
 //post a new user
-//clear database each time test lifecycle runs
 test('signup a new user', async () => {
     await request(app).post('/users').send({
         name : 'Rahul Kapoor',
         email : 'rahul123@gmail.com',
         password : 'qwerty123'
     }).expect(200)
-
 });
 
+//user login success
 test('log in existing user', async () => {
     await request(app).post('/users/login').send({
         email : userOne.email,
         password : userOne.password
     }).expect(200)
-
 });
 
+//user login failed
 test('log in existing user failed', async () => {
     await request(app).post('/users/login').send({
         email : userOne.email,
         password : 'failedlogin'
     }).expect(400)
-
 });
+
+//fetch user profile
+test('fetch user profile', async () => {
+    await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+} );
+
+//fetch user profile failed without setting the auth token
+test('fetch user profile', async () => {
+    await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
+} );
+
+//delete user success
+test('fetch user profile', async () => {
+    await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+} );
+
+//delete user failure without setting auth token
+test('fetch user profile', async () => {
+    await request(app)
+    .delete('/users/me')
+    .send()
+    .expect(401)
+} );
